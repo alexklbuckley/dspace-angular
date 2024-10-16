019254ba33e5814b22c61755ed2eff53451d451f
Wainui Witika-Park <wainuiwitikapark@catalyst.net.nz>
2024-10-17 12:37:47 +1300

    WR444476 tuwhera item page

--- src/themes/showcases/app/item-page/simple/item-types/untyped-item/untyped-item.component.ts
+++ src/themes/showcases/app/item-page/simple/item-types/untyped-item/untyped-item.component.ts
@@ -8,18 +8,47 @@ import { Context } from '../../../../../../../app/core/shared/context.model';
 import {
   UntypedItemComponent as BaseComponent
 } from '../../../../../../../app/item-page/simple/item-types/untyped-item/untyped-item.component';
+import {
+  AUTO_STYLE,
+  animate,
+  state,
+  style,
+  transition,
+  trigger
+} from '@angular/animations';
 
 /**
  * Component that represents an untyped Item page
  */
-@listableObjectComponent(Item, ViewMode.StandalonePage, Context.Any, 'custom')
+@listableObjectComponent(Item, ViewMode.StandalonePage, Context.Any, 'showcases')
 @Component({
   selector: 'ds-untyped-item',
-  // styleUrls: ['./untyped-item.component.scss'],
-  styleUrls: ['../../../../../../../app/item-page/simple/item-types/untyped-item/untyped-item.component.scss'],
-  // templateUrl: './untyped-item.component.html',
-  templateUrl: '../../../../../../../app/item-page/simple/item-types/untyped-item/untyped-item.component.html',
+  styleUrls: ['./untyped-item.component.scss'],
+  //styleUrls: ['../../../../../../../app/item-page/simple/item-types/untyped-item/untyped-item.component.scss'],
+  templateUrl: './untyped-item.component.html',
+  //templateUrl: '../../../../../../../app/item-page/simple/item-types/untyped-item/untyped-item.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
+  animations: [
+    trigger('collapse', [
+      state('false', style({ height: AUTO_STYLE, visibility: AUTO_STYLE })),
+      state('true', style({ height: '0', visibility: 'hidden' })),
+      transition('false => true', animate(300 + 'ms ease-in')),
+      transition('true => false', animate(300 + 'ms ease-out'))
+    ])
+  ]
 })
 export class UntypedItemComponent extends BaseComponent {
+  collapsed = false;
+
+  toggle() {
+    this.collapsed = !this.collapsed;
+  }
+
+  expand() {
+    this.collapsed = false;
+  }
+
+  collapse() {
+    this.collapsed = true;
+  }
 }
